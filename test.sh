80efb1da1e5067ad751467ca6ef1213afd8f5e2f
marcelhas <marcel.hasieber@gmail.com>
2023-05-08 19:33:10 +0200

    Replace logs with simple printfs

--- test.sh
+++ test.sh
@@ -164,11 +164,11 @@ log_not_ok() {
 
     printf "${RED}not ok %s${RESET} - %s\n" "$test_number" "$test_name"
     if [[ -n "${VERBOSE-}" ]]; then
-        log_err "---"
-        log_warn "Expected:"
-        log_succ "$expected"
-        log_warn "Actual:"
-        log_err "$actual"
+        printf "---\n"
+        printf "Expected:\n"
+        printf "%s\n" "$expected"
+        printf "Actual:\n"
+        printf "%s\n" "$actual"
     fi
 }
 
